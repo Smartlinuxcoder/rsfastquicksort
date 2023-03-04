e65f4253969f1f3eb9d4faf3cec4db2c46366795
Smartlinuxcoder <tu@esempio.com>
2023-03-04 16:38:55 +0100

    added func

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
     println!("Hello, world!");
-    let array: Vec<i32> = vec![3,2,8,9,1,4,6,5,7];
+    let array: Vec<i32> = vec![3,2,8,9,1,4,6,0,5,7];
+    quicksort(array);
 
+}
+
+fn quicksort(array:Vec<i32>) {
+    let pivot = array[array.len()-1];
 }
