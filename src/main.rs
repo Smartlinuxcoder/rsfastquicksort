c8b853dca55b5dc0e5482719e7a6a41e86e0a42d
Smartlinuxcoder <tu@esempio.com>
2023-03-04 16:58:22 +0100

    making algo

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,14 @@ fn main() {
 
 fn quicksort(array:Vec<i32>) {
     let pivot = array[array.len()-1];
+    let mut medium: Vec<i32> = array.clone();
+    for n in 0..=medium.len() {
+        if medium[n] < pivot {
+            medium.insert(0, medium[n]);
+            medium.remove(n);
+        } else {
+            medium.insert(medium.len()-1, medium[n]);
+            medium.remove(n);
+        }
+    }
 }
